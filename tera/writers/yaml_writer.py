77fbc08fcab062042a22bf91bb3cf9d54d1ad2cf
wolnei mignoni <vichsort@proton.me>
2025-12-30 16:53:05 -0300

    feat(writers): yaml writer

---
+++ tera/writers/yaml_writer.py
@@ -0,0 +1,26 @@
+import yaml
+from pathlib import Path
+from tera.domain.models import TeraSchema
+from tera.adapters.openapi import TeraOpenApiAdapter
+
+class YamlFileWriter:
+    """
+    Concrete implementation of TeraWriter.
+    Recieves the Schema, converts to YAML and saves.
+    """
+    def __init__(self, output_path: Path):
+        self.output_path = output_path
+
+    def write(self, schema: TeraSchema) -> None:
+        adapter = TeraOpenApiAdapter(schema)
+        openapi_dict = adapter.convert()
+
+        with open(self.output_path, 'w', encoding='utf-8') as f:
+            yaml.dump(
+                openapi_dict, 
+                f, 
+                sort_keys=False, 
+                allow_unicode=True, 
+                default_flow_style=False,
+                indent=2
+            )
